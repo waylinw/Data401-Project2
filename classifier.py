97933c653f209d7dc5ab9790893af74bc9bc3129
waylinw <waylinw@gmail.com>
2016-11-05 21:08:31 +0000

    correctly uses the tfidf vectorizer now, also looking for ways to reduce features and also more ways to extract features, like n-gram or idk, countVectorizer

--- classifier.py
+++ classifier.py
@@ -1,8 +1,8 @@
 class Classifier:
 
     def __init__(self):
-        """Use this to set and store the initial parameters that
-        you will later update and use to predict.
+        """Use this to set and store the parameters of your 
+        model that you will update and use to predict.
         """
         self.params = None
 
@@ -10,9 +10,10 @@ class Classifier:
         """Update the model based on new data.
         
         Args:
-            data: an array of Python dicts containing JSON
-              data of one or more comments. The
-              "controversiality" field is assumed to be 0 or 1.
+            data: a string containing the data in CSV
+              format. Fields appear in the exact same
+              order as they appear in the training data. 
+              This CSV does not contain a header row.
         """
         raise NotImplementedError
 
@@ -20,8 +21,15 @@ class Classifier:
         """Predict the controversiality of a new observation.
 
         Args:
-            data: an array of Python dicts containing JSON
-              data of one or more comments. The "upvotes",
-              "downvotes", and "controversiality" fields
-              should be assumed to be not set.
+            data: a string containing the data in CSV
+              format. Fields are in the exact same
+              order as they were in the training data.
+              Some fields (e.g., ups, controversiality)
+              will be blank. This CSV does not contain
+              a header row.
+
+        Returns:
+            a list containing the predictions (0 or 1).
+            There should be one prediction for each row
+            of the input CSV.
         """
