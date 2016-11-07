01fd736b1a831da3fc84b7c26e7abb960fe82491
waylinw <waylinw@gmail.com>
2016-11-07 11:44:27 +0000

    this commit is trying to get the parsing done, I've added the code to transform text body to tfidf then select the top 50K with the women scale

--- classifier.py
+++ classifier.py
@@ -1,63 +1,75 @@
 from sklearn.externals import joblib 
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LassoCV
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import ElasticNetCV
-
 from sklearn.cross_validation import cross_val_score 
 from sklearn.linear_model import LogisticRegression 
 from sklearn.linear_model import LogisticRegressionCV          
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import chi2
-from sklearn.feature_selection import SelectFromModel
 import operator
 from io import StringIO
 import csv
+
 class Classifier:
     def __init__(self):
         self.model=joblib.load('tfidftop50K.pkl')
         self.Vect=joblib.load('tfidfmixbigram.pkl')
         self.Selector=joblib.load('tfidfselector.pkl')
+        self.betas=self.model.coef_
 
     def update(self, data):
-        with open(data, newline="") as f:
-            reader = csv.reader(StringIO(data))
-            next(reader) # skip header row
-            while True:
-                total += 1
-                try:
-                    row = next(reader)
-                except StopIteration:
-                    break
-
-                # define response variable (controversiality)
+        reader = csv.reader(StringIO(data))
+        while True:
+            #total += 1
+            for row in reader:
+                    # define response variable (controversiality)
                 y = int(row[20])
 
-                # gets the body
+                    # gets the body
                 body = row[17]
-                betas=self.model.coef_
-                #need a list of 0s and 1s of whether or not unigram/bigram is in model
-                j=self.Vect.transform(body)
+                
+                #transform the body into features with tfidf vectorizer, then the 
+                # feature_id list for updating coefs
+                x_tfidf = self.Vect.transform(body)
+                x_train = self.Selector.transform(x_tfidf)
+                
+                # here will give you a list of indexes to update in the coef_
+                feature_idx = x_test.getrow(0).nonzero()[1]
+                
                 predict=self.model.intercept_
-                # get residual of current prediction
-                # Dictionary has key of feature id, value of lookup
-                # needs to be altered to reflect beta equations
-                for ids, value in j:
-                    if ids in dictionary:
-                        beta = betas[dictionary[ids]]
-                        predict+=beta*value
-                resid = y - (1 / (1 + np.exp(-predict)))
-                for i in j:
+                for ids in feature_idx:
+                    beta = betas[ids]
+                    predict += beta * value
+                
+                 resid = y - (1 / (1 + np.exp(-predict)))
+                 for i in j:
                     beta[j] += (1 / total) * resid
 
-                # determine whether prediction was correct
-                correct += 1 * (abs(resid) < .5)
-                # print out the accuracy rate every 1000 iterations
-                if total % 1000 == 0: print(correct / total)
+                    # determine whether prediction was correct
+                    #correct += 1 * (abs(resid) < .5)
+                    # print out the accuracy rate every 1000 iterations
+                    #if total % 1000 == 0: print(correct / total)
+                    #need a list of 0s and 1s of whether or not unigram/bigram is in model
+                    #j=self.Vect.transform(body)
+                    #predict=self.model.intercept_
+
+                    # get residual of current prediction
+                    # Dictionary has key of feature id, value of lookup
+                    # needs to be altered to reflect beta equations
+                    #for ids, value in j:
+                    #    if ids in dictionary:
+                    #        beta = betas[dictionary[ids]]
+                    #        predict+=beta*value
+                    #resid = y - (1 / (1 + np.exp(-predict)))
+                    #for i in j:
+                    #    beta[j] += (1 / total) * resid
+
+                    # determine whether prediction was correct
+                    #correct += 1 * (abs(resid) < .5)
+                    # print out the accuracy rate every 1000 iterations
+                    #if total % 1000 == 0: print(correct / total)
 
     def predict(self, data):
          with open(data, newline="") as f:
