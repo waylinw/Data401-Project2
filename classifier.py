70a16d97bf7671689fc305dc78693cf13e94b299
waylinw <waylinw@gmail.com>
2016-11-07 17:59:15 +0000

    classifier, csv reader still doens't work

--- classifier.py
+++ classifier.py
@@ -21,9 +21,11 @@ class Classifier:
 
     def update(self, data):
         reader = csv.reader(StringIO(data))
+        correct = 0
+        total = 0
         while True:
-            #total += 1
             for row in reader:
+                total += 1
                     # define response variable (controversiality)
                 y = int(row[20])
 
@@ -41,15 +43,15 @@ class Classifier:
                 
                 predict=self.model.intercept_
                 for ids in range(len(feature_index)):
-                    beta = betas[feature_index[ids]]
+                    beta = self.betas[feature_index[ids]]
                     predict += beta * feature_values[ids]
                 
                 resid = y - (1 / (1 + np.exp(-predict)))
                 for ids in feature_index:
-                    beta[feature_index] += (1 / total) * resid
+                    self.beta[feature_index] += (1 / total) * resid
                 
                 correct += 1 * (abs(resid) < .5)
-                if total % 1000 == 0: print(correct/total)
+            print(correct)
                     # determine whether prediction was correct
                     #correct += 1 * (abs(resid) < .5)
                     # print out the accuracy rate every 1000 iterations
@@ -90,22 +92,14 @@ class Classifier:
 
                 # gets the body
                 body = row[17]
-                betas=self.model.coef_
-                #need a list of 0s and 1s of whether or not unigram/bigram is in                 
-                j=Vect.transform(body)
-                predict=self.model.intercept_
-                # get residual of current prediction
-                # Dictionary has key of feature id, value of lookup
-                # needs to be altered to reflect beta equations
-                for ids, value in j:
-                    if ids in dictionary:
-                        beta = betas[dictionary[ids]]
-                        predict+=beta*value
-                resid = y - (1 / (1 + np.exp(-predict)))
-                for i in j:
-                    beta[j] += (1 / total) * resid
-
-                # determine whether prediction was correct
-                correct += 1 * (abs(resid) < .5)
-                # print out the accuracy rate every 1000 iterations
-                if total % 1000 == 0: print(correct / total)
+                
+                x_tfidf = self.Vect.transform(body)
+                x_train = self.Selector.transform(x_tfidf)
+                
+                # here will give you a list of indexes to update in the coef_
+                feature_index = x_train.getrow(0).nonzero()[1]
+                feature_values = x_train.getrow(0).nonzero()[0]
+                
+                
+                ## TO DO: Figure out how to calculate the next value. i think we have to calc the resid like above
+                ## and it's controversial when the (abs(resid) < .5)
